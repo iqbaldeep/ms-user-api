52860d6a385e2bcd442b625db7dadee11c3688bc
Singh, Iqbaldeep <iqbaldeep.singh5@t-mobile.com>
2017-05-07 15:37:31 -0700

    used StringUtils.isBlank rather than null check

--- src/main/java/com/demo/user/domain/EntityBuilder.java
+++ src/main/java/com/demo/user/domain/EntityBuilder.java
@@ -3,8 +3,10 @@ package com.demo.user.domain;
 import org.joda.time.LocalDate;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+import org.springframework.util.StringUtils;
 
 import com.demo.user.model.User;
+
 
 /**
  * builds entities from models using builder pattern
@@ -14,8 +16,8 @@ import com.demo.user.model.User;
 public class EntityBuilder {
 	
 	/**
-	 * creates UserEntity object from User model object
-	 * @param model
+	 * creates UserEntity object from User entity object
+	 * @param entity
 	 * @return
 	 */
 	public static UserEntity getUserEntity(User model){
@@ -32,11 +34,11 @@ public class EntityBuilder {
 	 * @param date
 	 * @return
 	 */
-	private static LocalDate convertToLocalDate(String date){
+	static LocalDate convertToLocalDate(String date){
 		LocalDate localdate = null;
 		
 		//parse only if date is non-null
-		if(null != date){
+		if( ! StringUtils.isEmpty(date)){
 			//create formatter as per input format
 			DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyy-MM-dd");
 			
