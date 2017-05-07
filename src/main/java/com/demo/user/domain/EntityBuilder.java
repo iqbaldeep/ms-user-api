e73ef2035acb1215c2515af6afe57f99bc03b574
Singh, Iqbaldeep <iqbaldeep.singh5@t-mobile.com>
2017-05-07 13:36:55 -0700

    added null checks

--- src/main/java/com/demo/user/domain/EntityBuilder.java
+++ src/main/java/com/demo/user/domain/EntityBuilder.java
@@ -6,10 +6,20 @@ import org.joda.time.format.DateTimeFormatter;
 
 import com.demo.user.model.User;
 
+/**
+ * builds entities from models using builder pattern
+ * @author Iqbaldeep_Singh
+ *
+ */
 public class EntityBuilder {
 	
+	/**
+	 * creates UserEntity object from User model object
+	 * @param model
+	 * @return
+	 */
 	public static UserEntity getUserEntity(User model){
-		
+		//build entity and return
 		return UserEntity.getBuilder()
 				.withFirstName(model.getFirstName())
 				.withLastName(model.getLastName())
@@ -17,10 +27,25 @@ public class EntityBuilder {
 				.build();
 	}
 	
+	/**
+	 * parses date sent by api consumer to format required by database
+	 * @param date
+	 * @return
+	 */
 	private static LocalDate convertToLocalDate(String date){
-		DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyy-MM-dd");
+		LocalDate localdate = null;
 		
-		return dtf.parseLocalDate(date);
+		//parse only if date is non-null
+		if(null != date){
+			//create formatter as per input format
+			DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyy-MM-dd");
+			
+			//convert date string to LocalDate
+			localdate = dtf.parseLocalDate(date);
+		}
+		
+		
+		return localdate;
 	}
 
 }
--- src/main/java/com/demo/user/model/ModelBuilder.java
+++ src/main/java/com/demo/user/model/ModelBuilder.java
@@ -4,10 +4,22 @@ import org.joda.time.LocalDate;
 
 import com.demo.user.domain.UserEntity;
 
+/**
+ * this class is used to convert all entities to models 
+ * It uses builder pattern to build new objects cleanly 
+ * @author Iqbaldeep_Singh
+ *
+ */
 public class ModelBuilder {
 	
+	/**
+	 * builds User model object from UserEntity
+	 * @param entity
+	 * @return
+	 */
 	public static User getUserModel(UserEntity entity){
 		
+		//build and return model
 		return User.getBuilder()
 				.withId(entity.getId())
 				.withFirstName(entity.getFirstName())
@@ -17,8 +29,20 @@ public class ModelBuilder {
 		
 	}
 	
+	/**
+	 * gets date in human readable form. This is to avoid any date conversions on frontend
+	 * @param date from DB
+	 * @return human readable date string in format MM/dd/yyyy
+	 */
 	private static String getReadableDate(LocalDate date){
-		return date.toString("MM/dd/yyyy");
+		String readableDate = null;
+		//convert only if non-null
+		if(null != date){
+			//convert date to readable form
+			readableDate = date.toString("MM/dd/yyyy");
+		}
+		
+		return readableDate;
 	}
 
 }
